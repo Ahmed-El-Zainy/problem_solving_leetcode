33b9cf309f7d86353563d36141407eed876b2aa6
Ahmed Mostafa <119751677+Ahmed-El-Zainy@users.noreply.github.com>
2026-01-18 14:42:09 +0200

    [Q1] [Time Beats: 100.00%] [Memory Beats: 14.36%] - LeetPush

--- Q1.-Number-of-Students-Unable-to-Eat-Lunch.py
+++ Q1.-Number-of-Students-Unable-to-Eat-Lunch.py
@@ -1,16 +1,21 @@
 1from collections import deque
 2class Solution:
 3    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-4        queue = deque(students)
-5        count = 0
-6        while queue and sandwiches:
-7            if queue[0]==sandwiches[0]:
-8                queue.popleft()
-9                sandwiches.pop(0)
-10                count = 0
-11            else:
-12                queue.append(queue.popleft())
-13                count += 1
-14                if count == len(queue):
-15                    return len(queue)
-16        return len(queue)
+4        """
+5        time complexity : O(n^2)
+6        memory: 19.41MB , Beats: 6.23%
+7        """
+8        queue = deque(students)
+9        sandwiches = deque(sandwiches)
+10        count = 0
+11        while queue and sandwiches:
+12            if queue[0]==sandwiches[0]:
+13                queue.popleft()
+14                sandwiches.popleft()
+15                count = 0
+16            else:
+17                queue.append(queue.popleft())
+18                count += 1
+19                if count == len(queue):
+20                    return len(queue)
+21        return len(queue)
