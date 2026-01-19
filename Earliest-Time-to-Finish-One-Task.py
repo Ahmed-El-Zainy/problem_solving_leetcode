25878bb18a57d89aafc739a8bc210416e579ed2e
Ahmed Mostafa <119751677+Ahmed-El-Zainy@users.noreply.github.com>
2026-01-19 13:01:11 +0200

    [3683] [Time Beats: 38.83%] [Memory Beats: 17.77%] - LeetPush

---
+++ Earliest-Time-to-Finish-One-Task.py
@@ -0,0 +1,6 @@
+1class Solution:
+2    def earliestTime(self, tasks: List[List[int]]) -> int:
+3        """
+4        time complexity : O(1)
+5        """
+6        return min([x + y for x, y in tasks]) if tasks else 0
