220d299615e8913bfd1334693422c6facd091f8c
Ahmed Mostafa <119751677+Ahmed-El-Zainy@users.noreply.github.com>
2026-01-21 17:28:28 +0200

    [Q2] [Time Beats: 94.75%] [Memory Beats: 32.17%] - LeetPush

--- Q2.-License-Key-Formatting.py
+++ Q2.-License-Key-Formatting.py
@@ -1,5 +1,7 @@
 1class Solution:
 2    def licenseKeyFormatting(self, s: str, k: int) -> str:
-3        s= s.replace("-", "").upper()[::-1]
-4        groups = [s[i:i+k] for i in range(0,len(s), k)]
-5        return "-".join(groups)[::-1]
+3        s = s.replace("-", "").upper()[::-1]
+4        result = []
+5        for i in range(0, len(s), k):
+6            result.append(s[i:i+k])            
+7        return "-".join(result)[::-1]
