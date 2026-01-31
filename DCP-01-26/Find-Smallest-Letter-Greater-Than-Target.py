0d65031c6dde4677f0be20dc316c36270386a584
Ahmed Mostafa <119751677+Ahmed-El-Zainy@users.noreply.github.com>
2026-01-31 17:53:55 +0200

    [744] [Time Beats: 2.04%] [Memory Beats: 25.32%] - LeetPush

--- DCP-01-26/Find-Smallest-Letter-Greater-Than-Target.py
+++ DCP-01-26/Find-Smallest-Letter-Greater-Than-Target.py
@@ -1,10 +1,19 @@
 1class Solution:
 2    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-3        left, right = 0, len(letters) - 1
-4        while left <= right:
-5            mid = (left + right) // 2
-6            if letters[mid] <= target:
-7                left = mid + 1
-8            else:
-9                right = mid - 1
-10        return letters[left] if left < len(letters) else letters[0]
+3        n = len(letters)
+4        low = 0; high = n-1
+5        min1 = "z" 
+6        count = 0
+7        while low<=high:
+8            mid = (low+high)//2
+9            if letters[mid] > target:
+10                min1 = min(letters[mid],min1)
+11                high = mid - 1
+12                count = 1
+13            else:
+14                low = mid + 1
+15        if count == 0:
+16            return letters[0]
+17        else:
+18            return min1
+19        
