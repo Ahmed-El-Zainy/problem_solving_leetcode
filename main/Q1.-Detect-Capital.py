75862448a121d548d0d1430c07ceb2017a6b0534
Ahmed El Zainy <ahmedelzainy44@gmail.com>
2026-02-19 21:31:17 +0200

    new updates to the code

--- main/Q1.-Detect-Capital.py
+++ main/Q1.-Detect-Capital.py
@@ -1,3 +1,10 @@
 1class Solution:
 2    def detectCapitalUse(self, word: str) -> bool:
-3        return word.islower() or word.isupper() or (word[0].isupper() and word[1:].islower())
+3        """
+4        Capital usage is correct ONLY IF one of these holds:
+5        All letters are uppercase → "USA"
+6        All letters are lowercase → "leetcode"
+7        Only first letter is uppercase AND the rest are lowercase → "Google"
+8        """
+9        return word.isupper() or word.islower() or (word[0].isupper() and word[1:].islower())
+10
